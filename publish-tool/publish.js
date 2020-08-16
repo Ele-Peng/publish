8b78e89d1c9157aec23ce60fee0ef600b8ef49e4
ElePeng <374237925@qq.com>
2020-08-16 18:12:13 +0800

    feat: write cat.jpg to publish-tool

--- publish-tool/publish.js
+++ publish-tool/publish.js
@@ -1,37 +1,49 @@
 const http = require("http");
+const fs = require('fs');
 const querystring = require("querystring");
 
 const postData = querystring.stringify({
     "content": "Hello World!2222"
 })
 
-const options = {
-    host: "localhost",
-    port: 8081,
-    path: "/?filename=z.html",
-    method: "POST",
-    headers: {
-        "Content-Type": "application/octet-stream",
-        "Content-Length": Buffer.byteLength(postData)
-    }
-};
+let filename = "./cat.jpg"
+fs.stat(filename, (error, stat) => {
+    console.log(stat);
 
-const req = http.request(options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    /*res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-        console.log(`BODY: ${chunk}`);
+    const options = {
+        host: "localhost",
+        port: 8081,
+        path: "/?filename=cat.jpg",
+        method: "POST",
+        headers: {
+            "Content-Type": "application/octet-stream",
+            "Content-Length": stat.size
+        }
+    };
+
+    const req = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        /*res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });*/
     });
-    res.on('end', () => {
-        console.log('No more data in response.');
-    });*/
-});
-
-req.on('error', (e) => {
-    console.error(`problem with request: ${e.message}`);
-});
-
-// Write data to request body
-req.write(postData);
-req.end();
+    
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+    
+    let readStream = fs.createReadStream("./cat.jpg");
+    readStream.pipe(req);
+    readStream.on("end", () => {
+        req.end();
+    });
+    
+    // Write data to request body
+    // req.write(postData);
+    // req.end();
+})
